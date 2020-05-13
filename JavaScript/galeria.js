92c2e0c604cd4a66b6572f2f8427c4b1fad1c706
DianaTixi <tixidiana2@gmail.com>
2020-05-13 04:09:42 -0500

    cambios

--- JavaScript/galeria.js
+++ JavaScript/galeria.js
@@ -1,4 +1,4 @@
-// Declaración de variables.
+/* // Declaración de variables.
 var VFotos; // Lugar donde se almacenara la ruta de la imagen.
 var V_max; // Longitud máxima del vector "Fotos".
 var aux;   // Auxiliar para configuración.
@@ -18,45 +18,91 @@ VFotos [9] = "../Imagenes/img10.jpg"
 
 V_max = VFotos.length; // Obtiene la longitud del vector.
 V_max = V_max-1; // Diminuye uno su valor dado que V_max coge un valor a más.
+/////////////
+*/
 
+function aleatorios (){
+VFotos = new Array(); // Crea el vector para las fotos.
+VFotos [0] = "../Imagenes/img1.jpg"
+VFotos [1] = "../Imagenes/img2.jpg"
+VFotos [2] = "../Imagenes/img3.jpg"
+VFotos [3] = "../Imagenes/img4.jpg"
+VFotos [4] = "../Imagenes/img5.jpg"
+VFotos [5] = "../Imagenes/img6.jpg"
+VFotos [6] = "../Imagenes/img7.jpg"
+VFotos [7] = "../Imagenes/img8.jpg"
+VFotos [8] = "../Imagenes/img9.jpg"
+VFotos [9] = "../Imagenes/img10.jpg"
+vector= new Array();
+var n;
+for (var i =0; i <6; i++){
+    n = Math.round(Math.random()*(10.0));
+    vector[i]= n;
+    vector[i]++;
+    vector[i]= VFotos[n]
+}
+console.log(vector) 
+document.getElementById('b2').disabled=false;
+document.getElementById('b1').disabled=false;
+}
+var aux;   
+var aux_2=0;
 function siguiente () {
-			
-    if (aux_2 == V_max){
+    console.log(vector);
+    if (aux_2 == vector.length){
         aux_2 = 0;
     }
     else{
         aux_2 = aux_2+1;
     }
-    document.images.Diapositivas.src = VFotos[aux_2];
-        if(VFotos[aux_2] == VFotos[9]){
+    document.images.Diapositivas.src = vector[aux_2];
+        if(vector[aux_2] == vector[5]){
             document.getElementById('b2').disabled =true;
             console.log('desactivado') 
         }
 }
+
 function anterior () {
+    console.log(vector);
     if (aux_2 == 0){
-        aux_2 = V_max;			
+        aux_2 = vector.length;			
     }
     else{
         aux_2 = aux_2-1;
     }
-    document.images.Diapositivas.src = VFotos[aux_2];
-    document.images.Diapositivas.src = VFotos[aux_2];
-        if(VFotos[aux_2] == VFotos[0]){
+    document.images.Diapositivas.src = vector[aux_2];
+    document.images.Diapositivas.src = vector[aux_2];
+        if(vector[aux_2] == vector[0]){
             document.getElementById('b1').disabled =true;
             console.log('desactivado') 
             document.getElementById('b2').disabled=false;
     }
 }
-
+///////////////////////////////////
+/*function siguiente () {
+    if (aux_2 == longV){
+        aux_2 = 0;
+    }
+    else{
+        aux_2 = aux_2+1;
+    }
+    document.images.Diapositivas.src = vector[aux_2];
+        if(vector[aux_2] == vector[4]){
+            document.getElementById('b2').disabled =true;
+            console.log('desactivado') 
+        }
+}
+////////////////////////
 function aleatorio (){
+    VAux= new Array(6);
     for ( var i = 0; i <6; i++) {
     var azar = Math.round(Math.random()*(VFotos.length ));
     document.images.Diapositivas.src = VFotos[azar];
+    console.log("numero: "+azar)
     }
     setTimeout("aleatorio()",500);
 }
-
+/*
 function randomico(){
     for (var i=1; i<6; i++) {
         //var aleatorio = Math.floor(Math.random()*11)+1;
@@ -95,5 +141,3 @@ function randomico(){
         setTimeout( 5000);
         i++;
       }*/     
-
-
